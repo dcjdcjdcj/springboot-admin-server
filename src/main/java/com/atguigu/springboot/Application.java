99749e78e3f272aaaa0b8984194a4c553ac5e57d
dcjdcjdcj <2953212307@qq.com>
2020-10-27 22:49:15 +0800

    分之

--- src/main/java/com/atguigu/springboot/Application.java
+++ src/main/java/com/atguigu/springboot/Application.java
@@ -14,6 +14,8 @@ public class Application {
         System.out.println("1");
         System.out.println("3");
         System.out.println("4");
+
         System.out.println("5");
+        System.out.println("6");
     }
 }
