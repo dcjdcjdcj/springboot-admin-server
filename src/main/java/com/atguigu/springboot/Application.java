7acbafc1d4ccfe4dd62243ea5737f985398a351c
dcjdcjdcj <2953212307@qq.com>
2020-10-27 22:42:22 +0800

    分之

--- src/main/java/com/atguigu/springboot/Application.java
+++ src/main/java/com/atguigu/springboot/Application.java
@@ -1,6 +1,7 @@
 package com.atguigu.springboot;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,6 +10,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class,args);
-
+        System.out.println("3");
     }
 }
